ca990f58e463021cffcc1cb58ba1c9acc07ff4b7
jsamp701 <jsamp701@gmail.com>
2015-10-30 10:50:08 -0400

    Working on adding Controllers, Services, and Providers

---
+++ Predator.Api/Controllers/StaffController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Predator.Api.Controllers
+{
+    public class StaffController : ApiController
+    {
+        // GET: api/Staff
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Staff/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Staff
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Staff/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Staff/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
